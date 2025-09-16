3b378e512f79fcbe03da1744280694e8cc963c43
MidianKelly <senac.midiankellysantos@gmail.com>
2025-09-15 21:53:04 -0300

    feat(backend): #9: adiciona entidade de categoria.

--- backend/MeuCorre.Domain/Entities/Categoria.cs
+++ backend/MeuCorre.Domain/Entities/Categoria.cs
@@ -1,29 +1,52 @@
 ﻿using MeuCorre.Domain.Enums;
+using System;
 
 namespace MeuCorre.Domain.Entities
 {
     public class Categoria : Entidade
     {
+        public Guid UsuarioId { get; private set; }
         public string Nome { get; private set; }
         public TipoTransacao Tipo { get; private set; }
-        public string Descricao { get; private set; }
-        public string Cor { get; private set; }
-        public string Icone { get; private set; }
-        public Guid UsuarioId { get; private set; }
+        public string? Descricao { get; private set; }
+        public string? Cor { get; private set; }
+        public string? Icone { get; private set; }
+        public bool  Ativo { get; set; }
+
         //Propriedade de navegação para a entidade Usuario pois o usuário pode ter várias categorias
         public virtual Usuario Usuario { get; private set; }
 
 
-        public Categoria(string nome, TipoTransacao tipo, string descricao,string cor, string icone, Guid usuarioId)
+        public Categoria(Guid usuarioId, string nome, TipoTransacao tipo, string? descricao,string? cor, string? icone)
         {
-           
-            Nome = nome;
-            Tipo = tipo;
+            UsuarioId = usuarioId;
+            Nome = nome.ToUpper();
             Descricao = descricao;
             Cor = cor;
-            Icone= icone;
-            UsuarioId = usuarioId;
+            Icone = icone;
+            Tipo = tipo;
+            Ativo = true;
 
+        }
+        public void AtualizarInformacoes (string nome, TipoTransacao tipo, bool ativo, string? descricao, string? cor, string? icone)
+        {
+            Nome = nome.ToUpper();
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;
+            Tipo = tipo;
+            Ativo = ativo;
+            AtualizarDataMoficacao();
+        }
+        public void Ativar()
+        {
+            Ativo = true;
+            AtualizarDataMoficacao();
+        }
+        public void Inativar()
+        {
+            Ativo = false;
+            AtualizarDataMoficacao();
         }
 
     }
