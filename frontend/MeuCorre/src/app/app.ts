e7a731f0e016b0c8566298b5e002f134c8f07900
MidianKelly <senac.midiankellysantos@gmail.com>
2025-10-30 21:50:00 -0300

    feat:  modificando o front da tela

--- frontend/MeuCorre/src/app/app.ts
+++ frontend/MeuCorre/src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIcons],
   templateUrl: './app.html',
+  viewProviders: [provideIcons({ featherAirplay, heroUsers })],
   styleUrl: './app.css'
 })
 export class App {
